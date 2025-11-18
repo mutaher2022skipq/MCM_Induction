0a3b16b83f59c84d14d7fe2a423a505f6ac31808
Joseph Jana <mutaher.khan.skipq@gmail.com>
2025-11-18 22:51:07 +0500

    Fix ALLOWED_HOSTS for Railway and update Procfile to use PORT env var

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn mcm_admission.wsgi --log-file -
+web: gunicorn mcm_admission.wsgi --bind 0.0.0.0:$PORT --log-file -
 worker: celery -A mcm_admission worker -l info
 
--- mcm_admission/settings.py
+++ mcm_admission/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'changeme-insecure-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost,.railway.app,.up.railway.app').split(',')
 
 
 

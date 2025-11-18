44c0778b7f937899889f8381be9d8b16724e51af
Joseph Jana <mutaher.khan.skipq@gmail.com>
2025-11-18 23:57:31 +0500

    Add verbose logging to startup script and static file serving fallback

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+echo "Running collectstatic..."
 python manage.py collectstatic --noinput
+echo "Collectstatic completed. Starting Gunicorn..."
 exec gunicorn mcm_admission.wsgi --bind 0.0.0.0:$PORT --log-file -
 
